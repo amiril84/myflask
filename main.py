8118c71fe294ed860d2e93f758610489c2fd9f82
Amiril Muslimin <hi.amiril@gmail.com>
2024-07-05 14:41:12 +0700

    Update main.py --add hello

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({"hello world": "amiril"})
+
 if __name__ == '__main__':
   app.run(port=5000)
