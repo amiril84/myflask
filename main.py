6b8e4d4db6c34181680b034979668138222373aa
Amiril Muslimin <hi.amiril@gmail.com>
2024-07-05 15:01:17 +0700

    Update main.py --add checkandscrape

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+def check_and_scrape(url, num_words):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            text = ' '.join(soup.get_text(separator=' ', strip=True).split()[:num_words])
+            return text
+    except requests.RequestException as e:
+        return f"Error accessing {url}: {e}"
+
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -12,6 +22,12 @@ def index():
 def hello():
     return jsonify({"hello world": "amiril"})
 
+@app.route('/coba')
+def coba():
+    test_url = "https://almanhaj.or.id/84056-jenis-jenis-ibadah-haji.html"
+    num_words = 100
+    scraped_text = check_and_scrape(test_url, num_words)
+    return jsonify({"scraped_text": scraped_text})
 
 if __name__ == '__main__':
   app.run(port=5000)
